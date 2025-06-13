f1e054f90d31ff995770f613620da3aea42e0985
eduardon0 <eduardonoriega1994@gmail.com>
2025-06-13 16:05:08 -0600

    Update dashboard

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from dash import Dash
 from topic_extraction.dashboard_finalresults.layout import layout
 from topic_extraction.dashboard_finalresults.callbacks import register_callbacks
@@ -8,4 +9,6 @@ app.layout = layout
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 8050))  # Render sets PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
+    # app.run(debug=True, port=8080)
